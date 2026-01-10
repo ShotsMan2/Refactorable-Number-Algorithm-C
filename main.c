d04894e239f4aa85f7cee3fe1a41bc7c92e67630
Selim Efe <uysalselimefe@gmail.com>
2026-01-10 12:40:21 +0300

    Implement divisor counting logic for TAU number validation
    Added algorithmic logic to compute the total number of positive divisors and verify the divisibility condition required for Refactorable Numbers.

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,i,a;
+
+    a=1;
+
+    printf("sayi giriniz:");
+    scanf("%d",&x);
+
+    for(i=1;i<=x;i++){
+        if(x%i==0){
+            printf("%d.boleni:%d\n",a++,i);
+        }
+    }
+    printf("pozitif tam bolen sayisi = %d\n",--a);
+
+    if(x%a==0){
+        printf("%d sayisi TAU sayidir.\n",x);
+    }
+    else{
+        printf("%d sayisi TAU sayisi degildir.\n",x);
+    }
+
+    return 0;
+}
