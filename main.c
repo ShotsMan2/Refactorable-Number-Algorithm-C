137a0490223e7e9ce0325fd194a0da5a1e7b6290
Selim Efe <uysalselimefe@gmail.com>
2026-01-19 15:49:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,25 +3,29 @@
 
 int main()
 {
-    int x,i,a;
+    int x, i, a;
 
-    a=1;
+    a = 1;
 
     printf("sayi giriniz:");
-    scanf("%d",&x);
+    scanf("%d", &x);
 
-    for(i=1;i<=x;i++){
-        if(x%i==0){
-            printf("%d.boleni:%d\n",a++,i);
+    for (i = 1; i <= x; i++)
+    {
+        if (x % i == 0)
+        {
+            printf("%d.boleni:%d\n", a++, i);
         }
     }
-    printf("pozitif tam bolen sayisi = %d\n",--a);
+    printf("pozitif tam bolen sayisi = %d\n", --a);
 
-    if(x%a==0){
-        printf("%d sayisi TAU sayidir.\n",x);
+    if (x % a == 0)
+    {
+        printf("%d sayisi TAU sayidir.\n", x);
     }
-    else{
-        printf("%d sayisi TAU sayisi degildir.\n",x);
+    else
+    {
+        printf("%d sayisi TAU sayisi degildir.\n", x);
     }
 
     return 0;
